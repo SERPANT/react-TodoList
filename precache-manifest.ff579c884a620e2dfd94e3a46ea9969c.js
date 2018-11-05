110d36bc56ecb038a07794b4fecb4c54236dd70b
Shreejit <shreejit955@gmail.com>
2018-11-05 17:26:50 +0545

    Updates

---
+++ precache-manifest.ff579c884a620e2dfd94e3a46ea9969c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3cd14ade1062ab84bd8",
+    "url": "/react-TodoList/static/js/runtime~main.b3cd14ad.js"
+  },
+  {
+    "revision": "8bbe2d20985e5b0bfe4b",
+    "url": "/react-TodoList/static/js/main.8bbe2d20.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-TodoList/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "8bbe2d20985e5b0bfe4b",
+    "url": "/react-TodoList/static/css/main.5209cb9f.chunk.css"
+  },
+  {
+    "revision": "fe7206b0bd195014e0b7813001140395",
+    "url": "/react-TodoList/index.html"
+  }
+];
